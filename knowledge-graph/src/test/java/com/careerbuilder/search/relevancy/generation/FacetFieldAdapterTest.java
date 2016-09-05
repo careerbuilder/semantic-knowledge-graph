9ff8e80c6a903c019da0dc8ce29a8d52dbfc5b26
Trey Grainger <code@treygrainger.com>
2016-09-05 13:30:37 -0400

    Genericizing field names to be more user friendly

--- knowledge-graph/src/test/java/com/careerbuilder/search/relevancy/generation/FacetFieldAdapterTest.java
+++ knowledge-graph/src/test/java/com/careerbuilder/search/relevancy/generation/FacetFieldAdapterTest.java
@@ -27,71 +27,35 @@ public class FacetFieldAdapterTest {
     private List<String> fieldList;
     private String[] fieldNames = new String[]
             {
-                    "carotene.v1",
-                    "carotene.v1.top",
-                    "carotene.v2",
-                    "carotene.v2.top",
-                    "carotene.v2_2",
-                    "carotene.v2_2.top",
-                    "onet.v15",
-                    "onet.v15.top",
-                    "onet.v17",
-                    "onet.v17.top",
-                    "skills.v2",
-                    "skills.v2.top",
-                    "skills.v3",
-                    "skills.v3.top",
-                    "joblevel.v1",
-                    "carotene.v1.id-title",
-                    "carotene.v1.top.id-title",
-                    "carotene.v2.id-title",
-                    "carotene.v2.top.id-title",
-                    "carotene.v2_2.id-title",
-                    "carotene.v2_2.top.id-title",
-                    "onet.v15.id-title",
-                    "onet.v15.top.id-title",
-                    "onet.v17.id-title",
-                    "onet.v17.top.id-title",
-                    "skills.v2.did-title",
-                    "skills.v2.top.did-title",
-                    "skills.v3.did-title",
-                    "skills.v3.top.did-title",
-                    "joblevel.v1.level-description",
+                    "title",
+                    "title.top",
+                    "title.id-title",
+                    "title.top.id-title",
+                    "tags",
+                    "tags.top",
+                    "tags.id-title",
+                    "tags.top.id-title",
+                    "rank",
+                    "rank.level-description",
+                    "field.with.dots",
+                    "field.with.dots.id-title",
+                    "field.with.dots.id"
             };
 
     @Before
     public void init() {
         HashMap<String, String> paramMap = new HashMap<>();
-        paramMap.put("carotene.v1.facet-field", "id-title");
-        paramMap.put("carotene.v1.top.facet-field", "id-title");
-        paramMap.put("carotene.v2.facet-field", "id-title");
-        paramMap.put("carotene.v2.top.facet-field", "id-title");
-        paramMap.put("carotene.v2_2.facet-field", "id-title");
-        paramMap.put("carotene.v2_2.top.facet-field", "id-title");
-        paramMap.put("onet.v15.facet-field", "id-title");
-        paramMap.put("onet.v15.top.facet-field", "id-title");
-        paramMap.put("onet.v17.facet-field", "id-title");
-        paramMap.put("onet.v17.top.facet-field", "id-title");
-        paramMap.put("skills.v2.facet-field", "id-title");
-        paramMap.put("skills.v2.top.facet-field", "id-title");
-        paramMap.put("skills.v3.facet-field", "id-title");
-        paramMap.put("skills.v3.top.facet-field", "id-title");
-        paramMap.put("joblevel.v1.facet-field", "level-description");
-        paramMap.put("carotene.v1.key", "id");
-        paramMap.put("carotene.v1.top.key", "id");
-        paramMap.put("carotene.v2.key", "id");
-        paramMap.put("carotene.v2.top.key", "id");
-        paramMap.put("carotene.v2_2.key", "id");
-        paramMap.put("carotene.v2_2.top.key", "id");
-        paramMap.put("onet.v15.key", "id");
-        paramMap.put("onet.v15.top.key", "id");
-        paramMap.put("onet.v17.key", "id");
-        paramMap.put("onet.v17.top.key", "id");
-        paramMap.put("skills.v2.key", "id");
-        paramMap.put("skills.v2.top.key", "id");
-        paramMap.put("skills.v3.key", "id");
-        paramMap.put("skills.v3.top.key", "id");
-        paramMap.put("joblevel.v1.key", "level");
+        paramMap.put("title.facet-field", "id-title");
+        paramMap.put("title.key", "id");
+        paramMap.put("title.top.facet-field", "id-title");
+        paramMap.put("tags.facet-field", "id-title");
+        paramMap.put("tags.key", "id");
+        paramMap.put("tags.top.facet-field", "id-title");
+        paramMap.put("tags.top.key", "id");
+        paramMap.put("rank.facet-field", "level-description");
+        paramMap.put("rank.key", "level");
+        paramMap.put("field.with.dots.facet-field", "id-value");
+        paramMap.put("field.with.dots.key", "id");
 
         new MockUp<FieldChecker>()
         {
@@ -111,46 +75,46 @@ public class FacetFieldAdapterTest {
     public void testAdaptNonExistantField() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null, null, invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "carotene");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "bogus");
     }
 
 
     @Test
-    public void testAdaptCaroteneV1() throws IOException
+    public void testAdaptTitle() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null, null, invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "carotene.v1");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "title");
 
         String actual = target.field;
-        Assert.assertEquals("carotene.v1.id-title", actual);
+        Assert.assertEquals("title.id-title", actual);
     }
 
     @Test
-    public void testAdaptCaroteneV1Top()
+    public void testAdaptTitleTop()
     {
         NodeContext context = new NodeContext(new ParameterSet(null, null, invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "carotene.v1.top");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "title.top");
 
         String actual = target.field;
-        Assert.assertEquals("carotene.v1.top.id-title", actual);
+        Assert.assertEquals("title.top.id-title", actual);
     }
 
     @Test
-    public void testAdaptJobLevel() throws IOException
+    public void testAdaptRank() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null,null,invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "joblevel.v1");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "rank");
 
         String actual = target.field;
-        Assert.assertEquals("joblevel.v1.level-description", actual);
+        Assert.assertEquals("rank.level-description", actual);
     }
 
 
     @Test
-    public void getMapValue_JobLevel() throws IOException
+    public void getMapValue_Rank() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null, null, invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "joblevel.v1");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "rank");
 
         SimpleOrderedMap<Object> resultBucket = new SimpleOrderedMap<>();
         resultBucket.add("val", "99^testdescription");
@@ -161,10 +125,10 @@ public class FacetFieldAdapterTest {
     }
 
     @Test
-    public void getStringValue_JobLevel() throws IOException
+    public void getStringValue_Rank() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null,null,invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "joblevel.v1");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "rank");
 
         SimpleOrderedMap<Object> resultBucket = new SimpleOrderedMap<>();
         resultBucket.add("val", "99^testdescription");
@@ -174,10 +138,10 @@ public class FacetFieldAdapterTest {
     }
 
     @Test
-    public void getMapValue_Carotene() throws IOException
+    public void getMapValue_Title() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null,null,invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "carotene.v1");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "title");
 
         SimpleOrderedMap<Object> resultBucket = new SimpleOrderedMap<>();
         resultBucket.add("val", "99^testtitle");
@@ -188,10 +152,10 @@ public class FacetFieldAdapterTest {
     }
 
     @Test
-    public void getStringValue_Carotene() throws IOException
+    public void getStringValue_Title() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null,null,invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "carotene.v1");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "title");
 
         SimpleOrderedMap<Object> resultBucket = new SimpleOrderedMap<>();
         resultBucket.add("val", "99^testtitle");
@@ -201,10 +165,10 @@ public class FacetFieldAdapterTest {
     }
 
     @Test
-    public void getMapValue_Skills() throws IOException
+    public void getMapValue_Tags() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null,null,invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "skills.v2");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "tags");
 
         SimpleOrderedMap<Object> resultBucket = new SimpleOrderedMap<>();
         resultBucket.add("val", "99^testtitle");
@@ -216,13 +180,40 @@ public class FacetFieldAdapterTest {
 
 
     @Test
-    public void getStringValue_Skills() throws IOException
+    public void getStringValue_Tags() throws IOException
     {
         NodeContext context = new NodeContext(new ParameterSet(null,null,invariants));
-        FacetFieldAdapter target = new FacetFieldAdapter(context, "skills.v2");
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "tags");
 
         SimpleOrderedMap<Object> resultBucket = new SimpleOrderedMap<>();
         resultBucket.add("val", "99^testtitle");
+        String actual = target.getStringValue(resultBucket);
+
+        Assert.assertEquals("99", actual);
+    }
+
+    @Test
+    public void getMapValue_FieldWithDots() throws IOException
+    {
+        NodeContext context = new NodeContext(new ParameterSet(null,null,invariants));
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "field.with.dots");
+
+        SimpleOrderedMap<Object> resultBucket = new SimpleOrderedMap<>();
+        resultBucket.add("val", "99^testfieldvalue");
+        SimpleOrderedMap<String> actual = target.getMapValue(resultBucket);
+
+        Assert.assertEquals("99", actual.get("id"));
+        Assert.assertEquals("testfieldvalue", actual.get("value"));
+    }
+
+    @Test
+    public void getStringValue_FieldWithDots() throws IOException
+    {
+        NodeContext context = new NodeContext(new ParameterSet(null,null,invariants));
+        FacetFieldAdapter target = new FacetFieldAdapter(context, "field.with.dots");
+
+        SimpleOrderedMap<Object> resultBucket = new SimpleOrderedMap<>();
+        resultBucket.add("val", "99^testvalue");
         String actual = target.getStringValue(resultBucket);
 
         Assert.assertEquals("99", actual);
